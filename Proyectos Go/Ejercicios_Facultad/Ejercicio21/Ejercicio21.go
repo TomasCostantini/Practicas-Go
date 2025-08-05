1b6ca134d8a9d20904e1662963154c254f2f6d84
TomasCostantini <tomascostantini899@gmail.com>
2025-08-05 10:34:25 -0300

    .

---
+++ Proyectos Go/Ejercicios_Facultad/Ejercicio21/Ejercicio21.go
@@ -0,0 +1,21 @@
+/*
+	Guia de ejercicios GO:
+
+Ejercicio 21:
+Leer 5 números e indicar por cada uno si es múltiplos de 3 y no de 5. Para ello crear una función que se
+llame “verificarMultiplo” que reciba 2 parámetro:
+ El número que se desea verificar si es múltiplo de.
+ El múltiplo a verificar (3 o 5).
+La función deberá devolver como resultado un valor booleano indicando si es múltiplo o no. Para
+verificar si la multiplicidad se cumple se deberá realizar la división restando.
+El programa a desarrollar deberá implementar la función para resolver el problema planteado.
+*/
+package main
+
+func main() {
+
+}
+
+func verificarMultiplo() {
+
+}
