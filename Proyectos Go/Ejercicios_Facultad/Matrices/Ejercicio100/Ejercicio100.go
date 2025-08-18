f9d122257c717cfed7aaad21e748808b68f66beb
TomasCostantini <tomascostantini899@gmail.com>
2025-08-18 20:43:53 -0300

    .

--- Proyectos Go/Ejercicios_Facultad/Matrices/Ejercicio100/Ejercicio100.go
+++ Proyectos Go/Ejercicios_Facultad/Matrices/Ejercicio100/Ejercicio100.go
@@ -13,7 +13,6 @@ func main() {
 		A    [4][4]int
 		suma int
 	)
-	
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			fmt.Print("Ingrese el elemento [", i, "][", j, "]: ")
--- Proyectos Go/go.mod
+++
@@ -1,3 +0,0 @@
-module Ejercicio15
-
-go 1.24.3
