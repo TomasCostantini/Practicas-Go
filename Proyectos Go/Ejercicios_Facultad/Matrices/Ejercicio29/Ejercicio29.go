c0afaf4c2f87c54668be2d2a0ddbaa5962902d20
TomasCostantini <tomascostantini899@gmail.com>
2025-08-21 16:34:03 -0300

    Create Ejercicio29.go

---
+++ Proyectos Go/Ejercicios_Facultad/Matrices/Ejercicio29/Ejercicio29.go
@@ -0,0 +1,24 @@
+/*
+Guia de ejercicios GO.
+Ejercicio 29:
+Cargar una matriz A de 10x10 y crear una función que reciba como parámetro la matriz cargada y devuelva la
+suma de los elementos pares y la suma de los impares. Imprimir los resultados.
+*/
+package main
+
+//"fmt"
+//"math/rand"
+//"time"
+
+const (
+	filas    = 6
+	columnas = 5
+)
+
+func main() {
+
+}
+
+func ordenamiento() {
+
+}
