a8c0bfc02d7442cac278f46e865bfa7252e5bada
TomasCostantini <tomascostantini899@gmail.com>
2025-08-27 19:11:07 -0300

    .

---
+++ Proyectos Go/Ejercicios_Facultad/Matrices/Guia2/Ejercicio19.go
@@ -0,0 +1,11 @@
+/*
+Cargar una matriz A de 200x7, donde se almacenan las notas de 6 materias correspondientes a 200 alumnos.
+La primera columna indica el nro. de legajo del alumno y las restantes las notas de cada materia. Se pide:
+a) Generar una matriz M de orden Nx7, con los datos de los alumnos que tengan 60 puntos o más en todas las
+materias.
+b) Generar una matriz C de orden 200x8, similar a la matriz original colocando en la columna 8, el promedio
+obtenido por el alumno.
+c) Generar una función que ordene en forma ascendentemente por promedio la matriz C. La función deberá
+recibir como parámetro por referencia la matriz a ordenar.
+d) Imprimir la matriz C.
+*/
