08054d9f2ccad7ab8a9e6d8f79448b0d23d7d4ba
TomasCostantini <tomascostantini899@gmail.com>
2025-08-27 16:12:35 -0300

    Update Ejercicio29.go

--- Proyectos Go/Ejercicios_Facultad/Matrices/Ejercicio29/Ejercicio29.go
+++ Proyectos Go/Ejercicios_Facultad/Matrices/Ejercicio29/Ejercicio29.go
@@ -6,19 +6,51 @@ suma de los elementos pares y la suma de los impares. Imprimir los resultados.
 */
 package main
 
-//"fmt"
-//"math/rand"
-//"time"
-
-const (
-	filas    = 6
-	columnas = 5
+import (
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main() {
+const (
+	filas    = 10
+	columnas = 10
+)
 
+var suma_pares, suma_impares int
+
+func main() {
+	var (
+		A [filas][columnas]int
+	)
+
+	rand.Seed(time.Now().Unix())
+	//Cargar la matriz
+	for i := 0; i < filas; i++ {
+		for ii := 0; ii < columnas; ii++ {
+			A[i][ii] = rand.Intn(10)
+		}
+	}
+	for i := 0; i < filas; i++ {
+		for ii := 0; ii < columnas; ii++ {
+			fmt.Printf("%4d", A[i][ii])
+		}
+		fmt.Println("")
+	}
+	ordenamiento(A)
+	fmt.Println("La suma de los numeros pares es: ", suma_pares)
+	fmt.Println("La suma de los numeros impares es: ", suma_impares)
 }
 
-func ordenamiento() {
-
+func ordenamiento(A [filas][columnas]int) (sumapares, sumaimpares int) {
+	for i := 0; i < filas; i++ {
+		for ii := 0; ii < columnas; ii++ {
+			if A[i][ii]%2 == 0 {
+				suma_pares = suma_pares + A[i][ii]
+			} else {
+				suma_impares = suma_impares + A[i][ii]
+			}
+		}
+	}
+	return suma_pares, suma_impares
 }
