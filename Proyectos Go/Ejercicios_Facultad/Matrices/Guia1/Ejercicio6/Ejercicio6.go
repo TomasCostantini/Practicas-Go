a12717057af5dc6152084131e0303ae9fb42fcfc
TomasCostantini <tomascostantini899@gmail.com>
2025-09-03 16:22:42 -0300

    Update Ejercicio6.go

--- Proyectos Go/Ejercicios_Facultad/Matrices/Guia1/Ejercicio6/Ejercicio6.go
+++ Proyectos Go/Ejercicios_Facultad/Matrices/Guia1/Ejercicio6/Ejercicio6.go
@@ -1,4 +1,32 @@
-/*Guia de ejercicios GO
-Ejercicio 32:
+/*
+Guia de ejercicios GO
+Ejercicio 6:
 Cargar una matriz A de 4x4 elementos e imprimir aquellos elementos que se encuentren en filas pares.
 */
+package main
+
+import "fmt"
+
+const (
+	filas    = 4
+	columnas = 4
+)
+
+func main() {
+	var A [filas][columnas]int
+	for i := 0; i < filas; i++ {
+		for j := 0; j < columnas; j++ {
+			fmt.Print("Ingrese el elemento [", i, "][", j, "]: ")
+			fmt.Scan(&A[i][j])
+		}
+	}
+	fmt.Println("Elementos en filas pares:")
+	for i := 0; i < filas; i++ {
+		if i%2 == 0 {
+			for j := 0; j < columnas; j++ {
+				fmt.Print(A[i][j], " ")
+			}
+			fmt.Println()
+		}
+	}
+}
