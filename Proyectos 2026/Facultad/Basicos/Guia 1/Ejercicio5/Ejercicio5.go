be59dfd931a23160e4c003ef324af555619ec67d
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-06-12 10:56:05 -0300

    Create Ejercicio5.go

---
+++ Proyectos 2026/Facultad/Basicos/Guia 1/Ejercicio5/Ejercicio5.go
@@ -0,0 +1,20 @@
+/*Guia de ejercicios GO:
+Ejercicio 5:
+Leer un número entero y determinar si es par o impar. (utilizar %)
+*/
+
+package main
+
+import "fmt"
+
+var numero int
+
+func main() {
+	fmt.Println("Ingrese un numero")
+	fmt.Scanln(&numero)
+	if numero%2 == 0 {
+		fmt.Println("EL numero ingresado es par")
+	} else {
+		fmt.Println("El numero ingresado es impar")
+	}
+}
