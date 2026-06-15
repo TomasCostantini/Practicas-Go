d9f5a5bc0b81a9c4b57b1c76710d4c7117602cd6
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-06-15 01:05:15 -0300

    .

--- Proyectos 2026/Facultad/Basicos/Guia 1/Ejercicio14/Ejercicio14.go
+++ Proyectos 2026/Facultad/Basicos/Guia 1/Ejercicio14/Ejercicio14.go
@@ -4,3 +4,22 @@ Ejercicio 14:
 Leer un número entero e imprimir la suma de todos los números anteriores. Por ejemplo, si se ingresa el
 número 4, imprimir el resultado de: 1 + 2 + 3.
 */
+package maim
+
+import "fmt"
+
+var (
+	numero, suma int
+)
+
+func main(){
+	fmt.Println("Ingrese un numero entero: ")
+	fmt.Scan(&numero)
+
+	for i := numero; i >= 0; i-- {
+		suma = suma + i
+	}
+	fmt.Println("La suma total es: ")
+	fmt.Println(suma)
+	fmt.Println("Programa finalizado")
+}
