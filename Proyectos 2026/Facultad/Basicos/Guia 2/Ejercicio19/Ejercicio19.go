0141fcd64fb29e9056d2f0df37ed619c3827f348
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-06-19 19:27:46 -0300

    .

---
+++ Proyectos 2026/Facultad/Basicos/Guia 2/Ejercicio19/Ejercicio19.go
@@ -0,0 +1,23 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 19:
+Una empresa de viajes que realizó una campaña de promoción necesita calcular las comisiones que deberá
+pagar a sus promotores de ventas. Las mismas se calculan según la cantidad de excursiones vendidas por
+cada uno de ellos. Se decide asignar una categoría a cada vendedor según la cantidad vendida y según esa
+categoría se pagará un importe por cada excursión de acuerdo a la siguiente tabla:
+
+Categoría  Excursiones   $ x Excursión
+
+	A          <10             $1000
+	B       >= 10 y < 50       $1500
+	C       >= 50 y < 100      $1700
+	D          >= 100          $1900
+
+Se cuenta con N datos que corresponden a las cantidades vendidas por cada promotor. Se necesita
+conocer:
+• El importe a pagarle a cada promotor
+• La cantidad de promotores de cada categoría
+*/
+package main
+
+func main() {}
---
+++ Proyectos 2026/Facultad/Basicos/Guia 2/go.mod
@@ -0,0 +1,3 @@
+module Ejercicio15
+
+go 1.26.3
