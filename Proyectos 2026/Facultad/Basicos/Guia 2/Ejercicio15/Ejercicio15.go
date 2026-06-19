ebff6aac067fca661ec99458a2d671fc05b3bf80
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-06-19 17:28:33 -0300

    Update Ejercicio15.go

--- Proyectos 2026/Facultad/Basicos/Guia 2/Ejercicio15/Ejercicio15.go
+++ Proyectos 2026/Facultad/Basicos/Guia 2/Ejercicio15/Ejercicio15.go
@@ -13,17 +13,18 @@ import "fmt"
 
 var (
 	nombre, nombremayor, nombremenor                                         string
-	edad, edadmayor, edadmenor, cantidadmenor, cantidadentre, suma, promedio int
+	edad, edadmayor, edadmenor, cantidadmenor, cantidadentre, suma int
+	promedio float32 
 )
 
 const (
-	alumnos = 10
+	alumnos = 4
 )
 
 func main() {
 	for i := 0; i < alumnos; i++ {
 		fmt.Println("Ingrese el nombre del alumno: ")
-		fmt.Println(&nombre)
+		fmt.Scanln(&nombre)
 		fmt.Println("Ingrese la edad del alumno: ")
 		fmt.Scanln(&edad)
 
@@ -38,7 +39,7 @@ func main() {
 				nombremayor = nombre
 				edadmayor = edad
 			}
-			if edad > edadmenor {
+			if edad < edadmenor {
 				nombremenor = nombre
 				edadmenor = edad
 			}
@@ -51,11 +52,11 @@ func main() {
 		}
 		suma = suma + edad
 	}
-	promedio = edad / alumnos
+	promedio = float32(suma) / float32(alumnos)
 
-fmt.Println("El nombre del alumno de mayor edad es: ", nombremayor)
-fmt.Println("El nombre del alumno de menor edad es: ", nombremenor)
-fmt.Println("La cantidad de alumnos menores a 12 es: ", cantidadmenor)
-fmt.Println("La cantidad de alumnos entre 10 y 16 es: ", cantidadentre)
-fmt.Println("El promedio de edades es: ", promedio)
+	fmt.Println("El nombre del alumno de mayor edad es: ", nombremayor)
+	fmt.Println("El nombre del alumno de menor edad es: ", nombremenor)
+	fmt.Println("La cantidad de alumnos menores a 12 es: ", cantidadmenor)
+	fmt.Println("La cantidad de alumnos entre 10 y 16 es: ", cantidadentre)
+	fmt.Println("El promedio de edades es: ", promedio)
 }
