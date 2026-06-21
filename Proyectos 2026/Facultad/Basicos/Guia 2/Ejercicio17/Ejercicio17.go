f765fdee937076ae308cf7baabe5e6113099ac33
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-06-20 23:31:31 -0300

    Update Ejercicio17.go

--- Proyectos 2026/Facultad/Basicos/Guia 2/Ejercicio17/Ejercicio17.go
+++ Proyectos 2026/Facultad/Basicos/Guia 2/Ejercicio17/Ejercicio17.go
@@ -12,7 +12,8 @@ package main
 import "fmt"
 
 var (
-	votantes, partido, p1, p2, p3, mujeres, varonestres, genero int
+	votantes, partido, p1, p2, p3, mujeres, varonestres int
+	genero                                              string
 )
 
 func main() {
@@ -21,26 +22,35 @@ func main() {
 	for i := 0; i < votantes; i++ {
 		fmt.Println("Ingrese el partido elegido: ")
 		fmt.Scanln(&partido)
-		fmt.Println("Ingrese su genero: ")
+		fmt.Println("Ingrese su genero (H O M): ")
 		fmt.Scanln(&genero)
-		if partido == 1 {
-				if genero == 1 {
 
-			}
-			p1++
-		} else if partido == 2 {
-			if genero == 1 {
-
-			}
-
-		} else if partido == 3 {
-			if genero == 1 {
-				varonestres++
-			}
-
-		} else {
-			fmt.Println("Numero no valido")
+		if genero == "M" || genero == "m" {
+			mujeres++
 		}
 
+		if partido == 1 {
+			p1++
+
+		} else if partido == 2 {
+			p2++
+
+		} else if partido == 3 {
+			if genero == "M" || genero == "m" {
+				varonestres++
+			}
+			p3++
+		}
 	}
+	if p1 > p2 && p1 > p3 {
+		fmt.Println("El 1er partido es el ganador")
+	} else if p2 > p1 && p2 > p3 {
+		fmt.Println("El 2do partido es el ganador")
+	} else if p3 > p1 && p3 > p2 {
+		fmt.Println("El 3er partido es el ganador")
+
+	}
+	fmt.Println("La cantidad de mujeres que votaron es: ", mujeres)
+	fmt.Println("La cantidad de varones que votaron al 3er candidato es: ", varonestres)
+	fmt.Println("Programa finalizado")
 }
