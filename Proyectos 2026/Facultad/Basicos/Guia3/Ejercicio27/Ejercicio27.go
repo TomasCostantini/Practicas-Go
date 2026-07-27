5cd5a73d0888f91466ef1fdb60f2aba3d97b2c4c
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-07-27 20:30:07 -0300

    .

---
+++ Proyectos 2026/Facultad/Basicos/Guia3/Ejercicio27/Ejercicio27.go
@@ -0,0 +1,18 @@
+/*Guia de ejercicios GO:
+Ejercicio 20: 
+Se lee la siguiente información: Sexo, Estado Civil y Edad en donde: sexo podrá tomar los valores H o M,
+estado civil S (soltero), E (separado), C (casado) y V (viudo). Se pide:
+• Contar la cantidad de hombres casados mayores de 28 años.
+• Contar la cantidad de mujeres solteras menores de 25 años.
+• Calcular el promedio de edad de los hombres casados.
+• Contar cuantos hombres y mujeres se ingresaron.
+• Contar la cantidad de hombres viudos
+El final de la lectura viene indicado por un “S” en el campo sexo.
+*/
+
+package main
+import "fmt"
+
+func main(){
+
+}
