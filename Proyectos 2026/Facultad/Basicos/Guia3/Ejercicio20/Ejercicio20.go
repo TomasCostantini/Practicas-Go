b1a7617a7b5fa99546b79d06a166e065221f7a05
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-07-28 18:02:23 -0300

    Update Ejercicio20.go

--- Proyectos 2026/Facultad/Basicos/Guia3/Ejercicio20/Ejercicio20.go
+++ Proyectos 2026/Facultad/Basicos/Guia3/Ejercicio20/Ejercicio20.go
@@ -12,6 +12,55 @@ El final de la lectura viene indicado por un “S” en el campo sexo.
 
 package main
 
-func main() {
+import "fmt"
 
+var (
+	sexo, estado_civil            string
+	edad                          int
+	casados_mayores               = 0
+	mujeres_soteras_menores       = 0
+	promedio_edad_hombres_casados = 0
+	cantidad_hombres              = 0
+	cantidad_mujeres              = 0
+	hombres_viudos                = 0
+	suma_edad_casados             = 0
+)
+
+func main() {
+	fmt.Println("Ingrese el sexo de la persona (M o H): ")
+	fmt.Scan(&sexo)
+	for sexo != "S" || sexo != "s" {
+		fmt.Println("Ingrese la edad de la persona: ")
+		fmt.Scan(&edad)
+		fmt.Println("Ingrese el estado civil de la persona(S:soltero, C:casado,V: viudo): ")
+		fmt.Scan(&estado_civil)
+
+		if sexo == "H" || sexo == "h" {
+			cantidad_hombres++
+			if estado_civil == "v" || estado_civil == "V" {
+				hombres_viudos++
+			}
+			if estado_civil == "C" || estado_civil == "c" {
+				suma_edad_casados = suma_edad_casados + edad
+				if edad > 28 {
+					casados_mayores++
+
+				}
+
+			} else {
+				cantidad_mujeres++
+				if edad > 25 && estado_civil == "S" || estado_civil == "s" {
+					mujeres_soteras_menores++
+				}
+			}
+
+		}
+	}
+	fmt.Println("La cantidad total de hombres es: ", cantidad_hombres)
+	fmt.Println("La cantidad total de hombres casados mayores a 28 años es: ", casados_mayores)
+	fmt.Println("La edad promedio de los hombres casados es: ", promedio_edad_hombres_casados)
+	fmt.Println("La cantidad de hombre viudos es: ", hombres_viudos)
+	fmt.Println("La cantidad total de mujeres es: ", cantidad_mujeres)
+	fmt.Println("La cantidad total de mujeres solteras menores a 25 años es: ", mujeres_soteras_menores)
+	fmt.Println("Programa finalizado")
 }
