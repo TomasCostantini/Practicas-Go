9027c7821eeb2947554e3e81c7c3a2c34b3fc2f1
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-03 18:25:46 -0300

    Update Ejercicio27.go

--- Proyectos 2026/Facultad/Vectores/Ejercicio27/Ejercicio27.go
+++ Proyectos 2026/Facultad/Vectores/Ejercicio27/Ejercicio27.go
@@ -5,6 +5,35 @@ Cargar un vector de 10 elementos, ordenarlo en forma ascendente e imprimirlo ord
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const (
+		indice = 10
+	)
+	var (
+		vector [indice]int
+		n, aux int
+	)
+
+	for i := 0; i < indice; i++ {
+		fmt.Println("Ingrese un numero: ")
+		fmt.Scan(&n)
+		vector[i] = n
+	}
+
+	for i := 0; i < indice; i++ {
+		for z := 1; z < indice-1; z++ {
+			if vector[i] < vector[z] {
+				aux = vector[i]
+				vector[i] = vector[z]
+				vector[z] = aux
+			}
+		}
+	}
+	for i := 0; i < indice; i++ {
+		fmt.Println(vector[i])
+	}
+
+	fmt.Println("Programa finalizado")
 }
