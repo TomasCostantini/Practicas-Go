2f25ee22ec13cbbcce15d913c196baee9d52c31a
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-03 17:33:46 -0300

    Update Ejercicio25.go

--- Proyectos 2026/Facultad/Vectores/Ejercicio25/Ejercicio25.go
+++ Proyectos 2026/Facultad/Vectores/Ejercicio25/Ejercicio25.go
@@ -5,6 +5,27 @@ Cargar un vector de 8 elementos enteros e imprimir el mayor de ellos.
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		numeros  [8]int
+		n, mayor int
+	)
+
+	for i := 0; i < 8; i++ {
+		fmt.Println("Ingrese un numero: ")
+		fmt.Scan(&n)
+		numeros[i] = n
+	}
+
+	for i := 0; i < 8; i++ {
+		if i == 0 {
+			mayor = numeros[i]
+		} else {
+			if numeros[i] > mayor {
+				mayor = numeros[i]
+			}
+		}
+	}
 }
