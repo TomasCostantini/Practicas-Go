52e388098f2acf0f09fbee3d8a128de07e8513ef
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-03 20:04:58 -0300

    .

---
+++ Proyectos 2026/Facultad/Funciones y Procedimientos/Ejercicio1/Ejercicio1.go
@@ -0,0 +1,20 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 1:
+Leer los siguientes datos: cantidad de dólares a comprar y cantidad de pesos argentinos disponibles y
+cotización del día.
+Crear una función que devuelva un resultado booleado que indique si la cantidad de pesos con los que
+se cuentan alcanzan para efectuar la compra de dólares.
+La función recibirá 3 parámetros:
+ Cantidad pesos disponibles
+ Cantidad de dólar a comprar
+ Cotización del día
+El fin de ingreso de datos viene dado por la cantidad de pesos = 0.
+La cotización del día se debe ingresar una sola vez.
+Imprimir por cada juego de datos ingresados, si alcanza o no para realizar la operación de cambio.
+*/
+package main
+
+func main() {
+
+}
---
+++ Proyectos 2026/Facultad/Funciones y Procedimientos/Ejercicio3/Ejercicio3.go
@@ -0,0 +1,29 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 3:
+Una empresa de viajes que realizó una campaña de promoción necesita calcular las comisiones que
+deberá pagar a sus promotores de ventas. Las mismas se calculan según la cantidad de excursiones
+vendidas por cada uno de ellos. Se decide asignar una categoría a cada vendedor según la cantidad
+vendida y según esa categoría se pagará un importe por cada excursión de acuerdo a la siguiente tabla:
+
+Categoría         Excursiones            $ x Excursión
+A                   <10                     $1000
+B               >= 10 y < 100               $1500
+C                  >= 100                   $1900
+
+Se ingresan las cantidades de excursiones vendidas de 10 promotores. Se necesita conocer:
+a) la cantidad de promotores de cada categoría
+b) el importe a pagarle a cada promotor
+Resolver:
+ Para el punto a) crear una función “categoriaPromotor” que recibirá como parámetro la
+cantidad de excursiones que vendió y devuelva como resultado la letra de la categoría a la que
+pertenece.
+ Para el punto b) crear una función “importeAPagar” que recibirá como parámetro la cantidad
+de excursiones que vendió, esta función deberá hacer uso de la función “categoriaPromotor”
+para determinar el importe a pagar.
+Imprimir los resultados.
+*/
+package main
+
+func main() {
+}
