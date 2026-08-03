230c6823c0f4fae2e0f4cdf2377568d2bdc60b03
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-03 17:14:32 -0300

    Update Ejercicio23.go

--- Proyectos 2026/Facultad/Vectores/Ejercicio23/Ejercicio23.go
+++ Proyectos 2026/Facultad/Vectores/Ejercicio23/Ejercicio23.go
@@ -6,6 +6,27 @@ que aparece dicho número en el vector. Imprimir el resultado.
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		vector    [10]int
+		n, numero int
+		cuenta    = 0
+	)
+
+	fmt.Println("Ingrese el numero de referencia: ")
+	fmt.Scan(&n)
+	for i := 0; i < 10; i++ {
+		fmt.Println("Ingrese un numero: ")
+		fmt.Scan(&numero)
+		vector[i] = numero
+	}
+	for i := 0; i < 10; i++ {
+		if vector[i] == n {
+			cuenta++
+		}
+	}
+	fmt.Println("La cantidad de veces que n coincide es: ", cuenta)
+	fmt.Println("Programa finalizado")
 }
