87c1d0d3b1ce8ff36cebc93ba9ebcf9b3a8737c1
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-03 17:59:50 -0300

    Update Ejercicio26.go

--- Proyectos 2026/Facultad/Vectores/Ejercicio26/Ejercicio26.go
+++ Proyectos 2026/Facultad/Vectores/Ejercicio26/Ejercicio26.go
@@ -6,6 +6,28 @@ positivos de A. Imprimir el vector generado.
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		vectorA [10]int
+		vectorB []int
+		n       int
+		indiceB = 0
+	)
+	for i := 0; i < 10; i++ {
+		fmt.Println("Ingrese un numero: ")
+		fmt.Scan(&n)
+		vectorA[i] = n
+	}
+	for i := 0; i < 10; i++ {
+		if vectorA[i] > 0 {
+			vectorB[indiceB] = vectorA[i]
+			indiceB++
+		}
+	}
+
+	for i := 0; i < indiceB; i++ {
+		fmt.Print(vectorB[i])
+	}
 }
