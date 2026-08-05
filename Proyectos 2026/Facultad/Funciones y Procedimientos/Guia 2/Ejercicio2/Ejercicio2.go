163f2c93807a4ab8b8edae81d50310c750270722
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-05 19:13:10 -0300

    nashe

---
+++ Proyectos 2026/Facultad/Funciones y Procedimientos/Guia 2/Ejercicio2/Ejercicio2.go
@@ -0,0 +1,19 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 2:
+Realizar un programa que permita jugar un número indeterminado de veces a “Adivina el Número”
+que consiste en lo siguiente:
+● Genere un número aleatoriamente comprendido entre 0-100.
+● Permitir al jugador adivinar el número generado, para ello deberá introducir por teclado
+números mientras no acierte. Desarrollar una función que valide si el usuario adivinó el
+número o no, la función deberá recibir como parámetro el número ingresado por el
+usuario y el número generado aleatoriamente e informar por pantalla si el número
+generado es mayor o menor que el ingresado. Además deberá devolver un valor
+booleando que indique si acertó el número o no.
+●Contar el número de intentos.
+●En el momento de la victoria, mostrarlo en pantalla e informar el número de intentos que ha
+utilizado, sí el número de intentos es:
+o <= 5 se indica al jugador que es bueno.
+o 5 < número de intentos < 15 se indica al jugador que es regular.
+o el número de intentos es > 15 se indica que el jugador no es muy bueno.
+*/
