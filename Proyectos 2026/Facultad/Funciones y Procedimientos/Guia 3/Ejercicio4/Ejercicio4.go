0301327e2fced7451d804db0b446deec35420a14
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-12 18:33:46 -0300

    Update Ejercicio4.go

--- Proyectos 2026/Facultad/Funciones y Procedimientos/Guia 3/Ejercicio4/Ejercicio4.go
+++ Proyectos 2026/Facultad/Funciones y Procedimientos/Guia 3/Ejercicio4/Ejercicio4.go
@@ -10,13 +10,13 @@ Se pide:
 ● Crear una función que reciba como parámetro un número de móvil y el vector RES e imprima
 la cantidad total de cuadras realizadas y el importe recaudado por ese móvil.
 ● Utilizar la función del punto anterior para imprimir en pantalla el siguiente reporte:
-Nro. móvil Cant. total cuadras Importe recaudado
-1 100 50000
-2 150 75000
-3 90 45000
-4 200 100000
-5 205 102500
-6 90 45000
+Nro. móvil            Cant. total cuadras          Importe recaudado
+    1                        100                         50000
+    2                        150                         75000
+    3                        90                          45000
+	4 						 200 						 100000
+	5                        205                         102500
+	6                        90                          45000
 ● Realizar un procedimiento que permita ordenar un vector de forma descendente. El
 procedimiento deberá recibir como parámetro por referencia el vector a ordenar.
 ● Ordenar el vector REM con el procedimiento desarrollado en el punto anterior e imprimir el
