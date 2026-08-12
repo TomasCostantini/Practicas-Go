621c8f5f192ee8e451a4538db52a6c607a5c1dc4
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-12 18:30:05 -0300

    .

--- Proyectos 2026/Facultad/Funciones y Procedimientos/Guia 3/Ejercicio3/Ejercicio3.go
+++ Proyectos 2026/Facultad/Funciones y Procedimientos/Guia 3/Ejercicio3/Ejercicio3.go
@@ -1,11 +1,9 @@
 /*
 Guia de ejercicios GO:
 Ejercicio 3:
-Realizar un programa que lea dos números A y B y que le pregunte al usuario que operación desea
-realizar. Las operaciones son: 1 = sumar, 2 = restar, 3 = multiplicar, 4 = dividir, 0 = salir.
-Una vez que el usuario haya ingresado la operación a realizar y los números a operar, el programa
-deberá mostrar el resultado de la operación y volver a preguntar al usuario que desea hacer,
-mientras que el usuario no ingrese la opción 0 = salir, el programa deberá seguir funcionando.
-Cada operación deberá ser implementada con distintas funciones y deberá recibir como parámetro
-los números a operar y devolver el resultado de la operación.
+Se tienen 2 vectores A y B de 20 elementos. Programar un procedimiento que permita ordenar
+vectores de forma ascendente (aplicando el método de la burbuja). El procedimiento debe recibir
+como parámetro por referencia el vector a ordenar.
+Una vez ordenado los 2 vectores mediante la llamada al procedimiento, generar un nuevo vector C
+que sea la intercalación ordenada de A y de B (considere que no hay elementos repetidos).
 */
