9d4cdf61a2eab673826b01826e64265b933c6dbd
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-24 10:43:11 -0300

    .

---
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio1/Ejercicio1.go
@@ -0,0 +1,6 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 1:
+Cargar una matriz A de 4x4. Crear una función que reciba como parámetro la matriz cargada y devuelva como
+resultado la suma de sus elementos. Imprimir la suma.
+*/
