6ba9ffea527a49e5ed33a333704527b116eaecd3
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-24 20:10:04 -0300

    .

---
+++ Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio13/Ejercicio13.go
@@ -0,0 +1,12 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 13:
+Cargar una matriz A de 10x20 elementos. Generar una función que devuelva como resultado el mayor
+elemento de las filas pares y el menor de las filas impares y las posiciones donde se ubican. La función deberá
+recibir como parámetros:
+a) La matriz a buscar
+b) Si se desea buscar el mayor o menor (parámetro booleano)
+c) Si se desea buscar sobre filas pares o impares (parámetro booleano)
+Imprimir los resultados.
+*/
+package main
---
+++ Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio20/Ejercicio20.go
@@ -0,0 +1,16 @@
+/*
+Guia de ejercicios GO:
+Ejercicio 20:
+Una casa de artículos deportivos almacena en una matriz A de 100x4 los datos correspondientes a los 100
+artículos que vende, donde la primera columna contiene el código del artículo, la segunda el código de la
+marca del artículo, la tercera la cantidad existente y la cuarta el precio de venta. Se pide:
+a) Generar un procedimiento que reciba como parámetro la matriz A e imprimir el código de artículo y el
+precio correspondiente de aquellos cuya cantidad existente supere las cincuenta unidades.
+b) Generar una función que reciba como parámetro la matriz A y devuelva como resultado una matriz con el
+código del artículo y el código de marca de aquellos artículos cuyo precio sea menor o igual a $300.
+c) Generar una función que ordene en forma ascendente la matriz A de acuerdo al precio de venta. La
+función deberá recibir como parámetro por referencia la matriz a ordenar y el número de columna de la
+matriz por la que se desea ordenar.
+d) Generar una función que imprima la matriz recibida como parámetro. Utilizarla para imprimir la matriz A.
+*/
+package main
