336bee79edc506139f97e2abb24f57b5e118a6c1
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-24 19:27:26 -0300

    Update Ejercicio1.go

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio1/Ejercicio1.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio1/Ejercicio1.go
@@ -4,3 +4,4 @@ Ejercicio 1:
 Cargar una matriz A de 4x4. Crear una función que reciba como parámetro la matriz cargada y devuelva como
 resultado la suma de sus elementos. Imprimir la suma.
 */
+package main
