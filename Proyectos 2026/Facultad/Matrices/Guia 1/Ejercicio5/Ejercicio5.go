9125b5ac759282fda158a83162ac62e5edf36d52
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-25 18:24:27 -0300

    .

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio5/Ejercicio5.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio5/Ejercicio5.go
@@ -4,3 +4,37 @@ Ejercicio 5:
 Cargar una matriz de 6x6 elementos y poner un 0 en donde encuentre un valor par.
 */
 package main
+
+import "fmt"
+
+const (
+	filas    = 6
+	columnas = 6
+)
+
+func main() {
+	var (
+		numero int
+		matriz [filas][columnas]int
+	)
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			fmt.Println("Ingrese un numero: ")
+			fmt.Scan(&numero)
+			matriz[i][z] = numero
+		}
+	}
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			if matriz[i][z]%2 == 0 {
+				matriz[i][z] = 0
+			}
+		}
+	}
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			fmt.Print(matriz[i][z], "\t")
+		}
+		fmt.Println()
+	}
+}
---
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio5/go.mod
@@ -0,0 +1,3 @@
+module Ejercicio5
+
+go 1.26.3
