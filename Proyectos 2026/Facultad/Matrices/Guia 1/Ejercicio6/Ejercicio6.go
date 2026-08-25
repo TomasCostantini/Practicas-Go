a458adaddb7f1f9ea84718d182d0340c360632b1
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-25 18:33:19 -0300

    .

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio6/Ejercicio6.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio6/Ejercicio6.go
@@ -4,3 +4,33 @@ Ejercicio 6:
 Cargar una matriz A de 4x4 elementos e imprimir aquellos elementos que se encuentren en filas pares.
 */
 package main
+
+import "fmt"
+
+const (
+	filas    = 4
+	columnas = 4
+)
+
+func main() {
+	var (
+		numero int
+		A      [filas][columnas]int
+	)
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			fmt.Println("Ingrese un numero: ")
+			fmt.Scan(&numero)
+			A[i][z] = numero
+		}
+	}
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			if i%2 == 0 {
+				fmt.Print(A[i][z])
+			}
+		}
+		fmt.Println("")
+	}
+
+}
---
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio6/go.mod
@@ -0,0 +1,3 @@
+module Ejercicio6
+
+go 1.26.3
