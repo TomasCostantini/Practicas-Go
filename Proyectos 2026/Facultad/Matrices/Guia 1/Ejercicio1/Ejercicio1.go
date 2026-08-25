2a8ad708ade6c4ae484cea4088aa2f0f08a646c8
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-25 17:20:04 -0300

    Update Ejercicio1.go

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio1/Ejercicio1.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio1/Ejercicio1.go
@@ -23,7 +23,17 @@ func main() {
 		for z := 0; z < columnas; z++ {
 			fmt.Println("Ingrese un numero: ")
 			fmt.Scan(&numero)
-			A[i][z]=numero
+			A[i][z] = numero
 		}
 	}
+	fmt.Println("El valor de la suma de sus elementos es: ", sumar(A))
+}
+
+func sumar(A [filas][columnas]int) int {
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			suma = suma + A[i][z]
+		}
+	}
+	return suma
 }
