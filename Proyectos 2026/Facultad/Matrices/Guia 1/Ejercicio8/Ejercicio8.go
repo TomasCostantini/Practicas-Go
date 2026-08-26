04e3a095110e25f73d0e5061a18aaa3dd5d178d9
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-26 18:56:38 -0300

    Update Ejercicio8.go

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio8/Ejercicio8.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio8/Ejercicio8.go
@@ -4,3 +4,25 @@ Ejercicio 8:
 Cargar una matriz A de 3x5 e imprimir el mayor elemento.
 */
 package main
+
+import "fmt"
+
+const (
+	filas   = 3
+	columnas = 5
+)
+
+
+func main(){
+	var(
+		A[filas][columnas] int
+		mayor, numero int
+	)
+	for i:=0;i<filas;i++{
+		for z:=0;z<columnas;z++{
+			fmt.Println("Ingrese un numero: ")
+			fmt.Scan(&numero)
+			A[i][z]=numero
+		}
+	}
+}
