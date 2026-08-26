5ab13019e279b7cecdd66a10ab351807299c2c2b
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-08-26 18:51:34 -0300

    .

--- Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio7/Ejercicio7.go
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio7/Ejercicio7.go
@@ -11,3 +11,28 @@ const (
 	filas    = 5
 	columnas = 5
 )
+
+func main() {
+	var (
+		A        [filas][columnas]int
+		contador = 0
+		numero   int
+	)
+
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			fmt.Println("Ingrese un numero: ")
+			fmt.Scan(&numero)
+			A[i][z] = numero
+		}
+	}
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			if A[i][z] == i || A[i][z] == z {
+				contador++
+			}
+		}
+	}
+	fmt.Println("La cantidad de numeros iguales a las filas o columnas es: ", contador)
+
+}
---
+++ Proyectos 2026/Facultad/Matrices/Guia 1/Ejercicio7/go.mod
@@ -0,0 +1,3 @@
+module Ejercicio7
+
+go 1.26.3
