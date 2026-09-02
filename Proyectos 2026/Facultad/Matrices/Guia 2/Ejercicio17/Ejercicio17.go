6eadd5a6e5c0ecfa04b29e98094b349c6b8e71d9
TomasCostantini <114594166+TomasCostantini@users.noreply.github.com>
2026-09-02 18:45:39 -0300

    Update Ejercicio17.go

--- Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio17/Ejercicio17.go
+++ Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio17/Ejercicio17.go
@@ -4,3 +4,25 @@ Ejercicio 17:
 Cargar una matriz A de 5x5, ordenar cada fila en forma ascendente. Imprimir la matriz resultante.
 */
 package main
+
+import "fmt"
+
+const (
+	filas    = 5
+	columnas = 5
+)
+
+func main() {
+	var (
+		A      [filas][columnas]int
+		numero int
+	)
+
+	for i := 0; i < filas; i++ {
+		for z := 0; z < columnas; z++ {
+			fmt.Println("Ingrese un numero: ")
+			fmt.Scan(&numero)
+			A[i][z] = numero
+		}
+	}
+}
