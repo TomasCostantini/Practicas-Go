05d6ed0b525fb927a5c666c5a799d5482fdab5f7
TomasCostantini <tomascostantini899@gmail.com>
2026-09-13 17:01:39 -0300

    Update Ejercicio20.go

--- Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio20/Ejercicio20.go
+++ Proyectos 2026/Facultad/Matrices/Guia 2/Ejercicio20/Ejercicio20.go
@@ -15,102 +15,75 @@ d) Generar una función que imprima la matriz recibida como parámetro. Utilizar
 */
 package main
 
-import "fmt"
-
-const (
-	filas      int     = 4
-	columnas   int     = 7
-	materias   int     = 6
-	col        int     = 8
-	notaMinima float32 = 60
+import (
+	"fmt"
+	"math/rand"
+	"time"
 )
 
+const N = 100
+const M = 4
+
 func main() {
-	var (
-		A [filas][columnas]float32
-		C [filas][col]float32
-	)
-
-	for f := 0; f < filas; f++ {
-		fmt.Print("Ingrese el numero de legajo:")
-		fmt.Scanln(&A[f][0])
-		for c := 1; c < columnas; c++ {
-			fmt.Printf("Ingrese la nota de la %v° materia:", c)
-			fmt.Scanln(&A[f][c])
-		}
-	}
-	M := generarMatrizM(A)
-	C = generarMatrizC(A)
-	ordenarMatrizC(&C)
-	fmt.Println("La matriz de los que que Poseen un numero superior o igual a 60 en todas las materias")
-	for f := 0; f < len(M); f++ {
-		for c := 0; c < columnas; c++ {
-			fmt.Printf("|%4v |", M[f][c])
-		}
-		fmt.Println("")
-	}
-	fmt.Println("")
-	fmt.Println("La matriz ordenada en forma ascendete segun los promedios")
-	for f := 0; f < filas; f++ {
-		for c := 0; c < col; c++ {
-			fmt.Printf("|%4v |", C[f][c])
-		}
-		fmt.Println("")
+	var A [N][M]int
+	cargarMatriz(&A)
+	imprimirMatriz(A)
+	articulosConCantidadMayorACincuenta(A)
+	resultado := articulosConPrecioMenorOIgualA300(A)
+	imprimirMatrizGenerica(resultado)
+	ordenarPorPrecio(&A)
+	imprimirMatriz(A)
+}
+func cargarMatriz(matriz *[N][M]int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < N; i++ {
+		matriz[i][0] = rand.Intn(100) 
+		matriz[i][1] = rand.Intn(10)  
+		matriz[i][2] = rand.Intn(100) 
+		matriz[i][3] = rand.Intn(100) 
 	}
 }
-
-func generarMatrizM(A [filas][columnas]float32) (M [][columnas]float32) {
-	var (
-		mat    int
-		vector [columnas]float32
-	)
-	for f := 0; f < filas; f++ {
-		mat = 0
-		for c := 1; c < columnas; c++ {
-			if A[f][c] >= notaMinima {
-				mat++
-			}
-			if mat == materias {
-				vector[0] = A[f][0]
-				for c := 1; c < columnas; c++ {
-					vector[c] = A[f][c]
-				}
-				M = append(M, vector)
-			}
+func imprimirMatriz(matriz [N][M]int) {
+	for i := 0; i < N; i++ {
+		for j := 0; j < M; j++ {
+			fmt.Print(matriz[i][j], " ")
+		}
+		fmt.Println()
+	}
+}
+func articulosConCantidadMayorACincuenta(matriz [N][M]int) {
+	fmt.Println("Articulos con cantidad mayor a 50:")
+	for i := 0; i < N; i++ {
+		if matriz[i][2] > 50 {
+			fmt.Printf("Codigo de articulo: %d, Precio: %d\n", matriz[i][0], matriz[i][3])
 		}
 	}
-
-	return M
 }
-func generarMatrizC(A [filas][columnas]float32) (C [filas][col]float32) {
-	var (
-		suma float32
-	)
-	for f := 0; f < filas; f++ {
-		C[f][0] = A[f][0]
-		suma = 0
-		for c := 1; c < columnas; c++ {
-			suma += A[f][c]
-			C[f][c] = A[f][c]
+func articulosConPrecioMenorOIgualA300(matriz [N][M]int) [][2]int {
+	var resultado [][2]int
+	for i := 0; i < N; i++ {
+		if matriz[i][3] <= 300 {
+			resultado = append(resultado, [2]int{matriz[i][0], matriz[i][1]})
 		}
-		promedio := suma / float32(materias)
-		C[f][columnas] = promedio
 	}
-
-	return C
+	return resultado
 }
-
-func ordenarMatrizC(C *[filas][col]float32) {
-
-	for f1 := 0; f1 < filas-1; f1++ {
-		for f2 := f1 + 1; f2 < filas; f2++ {
-			if C[f1][7] > C[f2][7] {
-				for c := 0; c < col; c++ {
-					aux := C[f1][c]
-					C[f1][c] = C[f2][c]
-					C[f2][c] = aux
+func ordenarPorPrecio(matriz *[N][M]int) {
+	for i := 0; i < N-1; i++ {
+		for j := i + 1; j < N; j++ {
+			if matriz[i][3] > matriz[j][3] {
+				for k := 0; k < M; k++ {
+					matriz[i][k], matriz[j][k] = matriz[j][k], matriz[i][k]
 				}
 			}
 		}
+	}
+}
+func imprimirMatrizGenerica(matriz [][2]int) {
+	for i := 0; i < len(matriz); i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Print(matriz[i][j], " ")
+		}
+		fmt.Println()
 	}
 }
